94fe60969513a3538d8055fbdd57180d700d4dec
Bruhhhyann <mendozabryan486@gmail.com>
2026-03-02 11:09:05 +0800

    Add files via upload

---
+++ MySQL/Login.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQL
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            //get user and pass and remove extra spaces
+            string user = txtuser.Text.Trim();
+            string pass = txtpass.Text.Trim();
+            //check if user and pass is empty
+            if (user == "" || pass == "")
+            {
+                MessageBox.Show("Please Enter Username and Password ");
+                return; //stop the login process
+            }
+            //call db connection string
+            DBConnect db = new DBConnect();
+            try
+            {
+                //main function/s
+                db.Open();
+                //sql query to count matching user and pass
+                string query = "SELECT COUNT(*) FROM users WHERE " + "username=@username AND password=@password";
+                //create sql command
+                MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(query,db.Connection);
+                //add parameters to prevent sql injection
+                //@ used to get the record on localhost db
+                cmd.Parameters.AddWithValue("@username", user);
+                cmd.Parameters.AddWithValue("@password", pass);
+                //execute query and get results
+                int count = Convert.ToInt32(cmd.ExecuteScalar());
+                if (count ==1)
+                {
+                    //if one record found
+                    MessageBox.Show("Login Successful!");
+                    //open dashboard
+                    Dashboard dashboard = new Dashboard();
+                    dashboard.Show();
+                    this.Hide();
+
+
+
+                }
+                else
+                {
+                    //no accc found
+                    MessageBox.Show("Invalid User or Password");
+                }
+
+            }
+            catch (Exception ex) {        
+                //show error message
+                MessageBox.Show(ex.Message);
+
+
+            }
+            finally {
+                // close db
+                db.Close();
+            }
+
+            
+
+
+
+        }
+    }
+}
---
+++ MySQL/RegisterStudent.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace MySQL
+{
+    partial class RegisterStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(236, 114);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(206, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(236, 153);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(206, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(236, 260);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(206, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(147, 117);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 15);
+            label1.TabIndex = 4;
+            label1.Text = "FIRST NAME";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(149, 156);
+            label2.Name = "label2";
+            label2.Size = new Size(71, 15);
+            label2.TabIndex = 5;
+            label2.Text = "LAST NAME";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(163, 195);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 6;
+            label3.Text = "COURSE";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(165, 232);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 7;
+            label4.Text = "SECTION";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(185, 263);
+            label5.Name = "label5";
+            label5.Size = new Size(29, 15);
+            label5.TabIndex = 8;
+            label5.Text = "AGE";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(136, 65);
+            label6.Name = "label6";
+            label6.Size = new Size(185, 15);
+            label6.TabIndex = 9;
+            label6.Text = "REGISTER STUDENT IFORMATION";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(238, 192);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(204, 23);
+            comboBox1.TabIndex = 10;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(236, 229);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(204, 23);
+            comboBox2.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDark;
+            button1.Location = new Point(236, 289);
+            button1.Name = "button1";
+            button1.Size = new Size(206, 37);
+            button1.TabIndex = 12;
+            button1.Text = "SAVE RECORD";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // RegisterStudent
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "RegisterStudent";
+            Text = "RegisterStudent";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button1;
+    }
+}
