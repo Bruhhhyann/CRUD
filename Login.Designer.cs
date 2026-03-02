6de897c96d9b166db10c70f059b29077e4a0d0c5
Bruhhhyann <mendozabryan486@gmail.com>
2026-03-02 11:06:40 +0800

    Add files via upload

---
+++ Login.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MySQL
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtuser = new TextBox();
+            txtpass = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnlogin = new Button();
+            SuspendLayout();
+            // 
+            // txtuser
+            // 
+            txtuser.Location = new Point(138, 96);
+            txtuser.Name = "txtuser";
+            txtuser.Size = new Size(206, 23);
+            txtuser.TabIndex = 0;
+            txtuser.TextChanged += textBox1_TextChanged;
+            // 
+            // txtpass
+            // 
+            txtpass.Location = new Point(138, 140);
+            txtpass.Name = "txtpass";
+            txtpass.Size = new Size(206, 23);
+            txtpass.TabIndex = 1;
+            txtpass.UseSystemPasswordChar = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 99);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 15);
+            label1.TabIndex = 2;
+            label1.Text = "USERNAME";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 143);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 3;
+            label2.Text = "PASSWORD";
+            // 
+            // btnlogin
+            // 
+            btnlogin.BackColor = SystemColors.ControlDark;
+            btnlogin.Location = new Point(138, 169);
+            btnlogin.Name = "btnlogin";
+            btnlogin.Size = new Size(206, 37);
+            btnlogin.TabIndex = 4;
+            btnlogin.Text = "LOGIN";
+            btnlogin.UseVisualStyleBackColor = false;
+            btnlogin.Click += btnlogin_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(442, 287);
+            Controls.Add(btnlogin);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtpass);
+            Controls.Add(txtuser);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtuser;
+        private TextBox txtpass;
+        private Label label1;
+        private Label label2;
+        private Button btnlogin;
+    }
+}
---
+++ StudentList.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace MySQL
+{
+    partial class StudentList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(35, 136);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(700, 354);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(35, 55);
+            label6.Name = "label6";
+            label6.Size = new Size(185, 15);
+            label6.TabIndex = 10;
+            label6.Text = "REGISTER STUDENT IFORMATION";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(35, 83);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(206, 23);
+            textBox1.TabIndex = 11;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ControlDark;
+            button1.Location = new Point(247, 75);
+            button1.Name = "button1";
+            button1.Size = new Size(206, 37);
+            button1.TabIndex = 13;
+            button1.Text = "SEARCH";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ControlDark;
+            button2.BackgroundImageLayout = ImageLayout.None;
+            button2.Location = new Point(529, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(206, 37);
+            button2.TabIndex = 14;
+            button2.Text = "REFRESH";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(903, 97);
+            label1.Name = "label1";
+            label1.Size = new Size(187, 15);
+            label1.TabIndex = 15;
+            label1.Text = "UPDATE STUDENT INFORMATION";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(903, 136);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(206, 23);
+            textBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(903, 165);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(206, 23);
+            textBox3.TabIndex = 17;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(903, 194);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(206, 23);
+            textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(903, 281);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(206, 23);
+            textBox5.TabIndex = 19;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(903, 223);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(206, 23);
+            comboBox1.TabIndex = 20;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(903, 252);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(206, 23);
+            comboBox2.TabIndex = 21;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(824, 165);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 15);
+            label2.TabIndex = 22;
+            label2.Text = "FIRST NAME";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(826, 194);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 15);
+            label3.TabIndex = 23;
+            label3.Text = "LAST NAME";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(879, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(18, 15);
+            label4.TabIndex = 24;
+            label4.Text = "ID";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(846, 226);
+            label5.Name = "label5";
+            label5.Size = new Size(51, 15);
+            label5.TabIndex = 25;
+            label5.Text = "COURSE";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(842, 252);
+            label7.Name = "label7";
+            label7.Size = new Size(55, 15);
+            label7.TabIndex = 26;
+            label7.Text = "SECTION";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(868, 284);
+            label8.Name = "label8";
+            label8.Size = new Size(29, 15);
+            label8.TabIndex = 27;
+            label8.Text = "AGE";
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.ControlDark;
+            button3.Location = new Point(903, 310);
+            button3.Name = "button3";
+            button3.Size = new Size(206, 37);
+            button3.TabIndex = 28;
+            button3.Text = "SAVE RECORD";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.ControlDark;
+            button4.Location = new Point(903, 353);
+            button4.Name = "button4";
+            button4.Size = new Size(206, 37);
+            button4.TabIndex = 29;
+            button4.Text = "DELETE RECORD";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // StudentList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1156, 562);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(dataGridView1);
+            Name = "StudentList";
+            Text = "V";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label6;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label7;
+        private Label label8;
+        private Button button3;
+        private Button button4;
+    }
+}
